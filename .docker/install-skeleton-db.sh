777ceba469e9bd03e33b1938423e51adfd0e53b3
andreas-gruenwald <andreas.gruenwald@elements.at>
2022-08-01 23:41:26 +0200

    Create install-skeleton-db.sh

---
+++ .docker/install-skeleton-db.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ..
+export PIMCORE_INSTALL_ADMIN_USERNAME=root
+export PIMCORE_INSTALL_ADMIN_PASSWORD=elements
+export PIMCORE_INSTALL_MYSQL_USERNAME=pimcore
+export PIMCORE_INSTALL_MYSQL_PASSWORD=pimcore
+export PIMCORE_INSTALL_MYSQL_DATABASE=pimcore
+./vendor/bin/pimcore-install --mysql-host-socket=db --no-interaction --ignore-existing-config
